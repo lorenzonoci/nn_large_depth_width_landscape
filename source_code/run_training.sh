1579347a6a81278d92d7d783737a596f6dc24948
Alexandru Meterez <ameterez@login2.is.localnet>
2024-05-18 23:35:35 +0200

    updated code

--- source_code/run_training.sh
+++ source_code/run_training.sh
@@ -2,4 +2,9 @@
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate mup
 
-python main.py  --arch conv --width_mult $1 --depth_mult $2 --seed $3 --lr $4 --parametr $5 --save_dir=/home/ameterez/work/nips2024/top_hessian_many_longer2 --dataset cifar10  --epochs 25  --layers_per_block 1  --beta 1 --skip_scaling 0 --batch_size 256 --logging_steps 100 --warmup_steps 100 --top_hessian_eigvals 10 --eval_hessian
+# python main.py  --arch conv --width_mult $1 --depth_mult $2 --seed $3 --lr $4 --parametr $5 --optimizer muadam --save_dir=/home/ameterez/work/nips2024/understanding_adam_wider --dataset cifar10  --epochs 50  --layers_per_block $6  --beta 1 --batch_size 256 --logging_steps 100 --warmup_steps 100 --top_hessian_eigvals 1 --eval_hessian
+
+# python main.py  --arch vit --width_mult $1 --depth_mult $2 --seed $3 --lr $4 --parametr $5 --save_dir=/home/ameterez/work/nips2024/vit_training_without_lr_scaling --dataset cifar10  --epochs 20 --batch_size 64 --logging_steps 100 --optimizer muadam --num_workers 16 --warmup_steps 500 --top_hessian_eigvals 1 --norm ln --eval_hessian
+
+
+python main.py  --arch conv --width_mult $1 --depth_mult $2 --seed $3 --lr $4 --parametr $5 --optimizer muadam --save_dir=/home/ameterez/work/nips2024/understanding_multiple_layers_per_block_longer_with_adam_no_scaling --dataset cifar10  --epochs 50  --layers_per_block $6  --beta 1 --batch_size 256 --logging_steps 100 --warmup_steps 100 --top_hessian_eigvals 1 --eval_hessian
--- source_code/submit_training.py
+++ source_code/submit_training.py
@@ -4,16 +4,20 @@ from pycondor.pycondor import *
 import numpy as np
 
 if __name__ == '__main__':
-    seeds = [0, 1]
-    lrs = np.logspace(-2.5, 1.5, num=19)[4:17]
+    seeds = [74]
+    # lrs = np.logspace(-2.5, 1.5, num=19)[4:17] #sgd
+    # lrs = np.logspace(-3, -1, num=12) #adam with scaling
+    lrs = np.logspace(-4, -2, num=12) # adam without scaling
 
     # over width
-    depth_mult = 1
-    width_mults = [2, 4, 8, 16, 32]
-    params = ['mup']
-    for run in itertools.product(*[width_mults, seeds, lrs, params]):
+    depth_mults = [16,8,4,2,1]
+    width_mults = [0.5]
+    layers_per_blocks = [1, 2]
+    # layers_per_blocks = [4]
+    params = ['mup_sqrt_depth']
+    for run in itertools.product(*[width_mults, depth_mults, seeds, lrs, params, layers_per_blocks]):
         uid = uuid.uuid4().hex[:10]
-        arguments = f"{run[0]} {depth_mult} {run[1]} {run[2]} {run[3]}"
+        arguments = f"{run[0]} {run[1]} {run[2]} {run[3]} {run[4]} {run[5]}"
         output = f"runs/{uid}.stdout"
         error = f"runs/{uid}.stderr"
         log = f"runs/{uid}.log"
@@ -25,7 +29,7 @@ if __name__ == '__main__':
 
         try:
             content = make_submisison_file_content(executable, arguments, output, error, log, cpus, gpus, memory, disk)
-            run_job(uid, 300, content)
+            run_job(uid, 150, content)
         except:
             raise ValueError("Crashed.")
     print("Done.")
